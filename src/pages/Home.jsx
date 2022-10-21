e6ff3b3253af2896b65460d6eb85dd5347996de4
MehmetFK <mehmetfkuruldak@gmail.com>
2022-10-21 19:09:41 +0200

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from '../components/Header';
+import Input from '../components/Input';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Input />
+        </div>
+    );
+};
+
+export default Home;
